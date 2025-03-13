df6ca4b67da237fbdd2a29c35902b78b0afe25f8
carlo8767 <carlo8767@gmail.com>
2025-03-14 00:49:46 +0100

    djoskee

--- frog.py
+++ frog.py
@@ -1,25 +1,27 @@
 
  # ABSOLUTE ERROR 10-6
-def recover_input (self):
+def recover_input ():
 
-      number_lilies, possible_jumps  = input("Enter a values\n").split()
+      number_lilies, possible_jumps  = input().split()
       convert_jump = int(possible_jumps)
       key_nodes_probability = dict()
       # CREATE A DICTIONARY CONTAINS A LIST OF THE NODE WITH AN PERCENTAGE
       # ASSIGN THE PERCENTAGE STANDARD
-      for s in range(1, int(number_lilies)+1):
+      boundary = int(number_lilies)+1
+      for s in range(1, boundary):
          if s == 1:
              key_nodes_probability[s] = float(0)
          else:
              key_nodes_probability[s] = float(1)
-         sorted(key_nodes_probability.items())
       # BEGIN TO EVALUATE
       # IF THE EVENT OCCURS THE SAME TIME + OTHERWISE MULTIPLICATION
       # https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm Dijkstra
       while convert_jump > 0:
-        nodes, edge, prob_fall_down =  input("Enter a values\n").split()
+        nodes, edge, prob_fall_down =  input().split()
         convert_float = float(prob_fall_down)
-        for i in range(int(nodes)+1, int(edge)+1):
+        first_boundary = int(nodes)+1
+        second_boundary = int(edge)+1
+        for i in range(first_boundary, second_boundary):
             if convert_float < key_nodes_probability [i]:
                 key_nodes_probability [i] = convert_float
         convert_jump -= 1
@@ -32,11 +34,15 @@ def recover_input (self):
          variation = float(1) - v
          success *= float(variation)
          # ABSOLUTE ERROR 10 -6
-      print(f"{success:.6f}")
+      size = len(str(success))
+      if size > 6:
+        print(f'{success:.6f}')
+      else:
+        print( f'{success}')
 
 if __name__ == "__main__":
 
-    recover_input("")
+    recover_input()
 
 
 
