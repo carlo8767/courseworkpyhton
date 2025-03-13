4753153aa6dd95ea18840295abce2501e543e1d8
carlo8767 <carlo8767@gmail.com>
2025-03-13 23:43:00 +0100

    absolute

--- frog.py
+++ frog.py
@@ -1,29 +1,18 @@
 
-
-class Frog:
  # ABSOLUTE ERROR 10-6
-
- def __init__  (self, node, edge, probability_fall_down ):
-    self.node = node
-    self.edge = edge
-    self.probability_fall_down = probability_fall_down
-    self.next = None
-
-
 def recover_input (self):
 
       number_lilies, possible_jumps  = input("Enter a values\n").split()
       convert_jump = int(possible_jumps)
-      key_nodes = dict()
+      key_nodes_probability = dict()
       # CREATE A DICTIONARY CONTAINS A LIST OF THE NODE WITH AN PERCENTAGE
       # ASSIGN THE PERCENTAGE STANDARD
       for s in range(1, int(number_lilies)+1):
          if s == 1:
-             key_nodes[s] = float(0)
+             key_nodes_probability[s] = float(0)
          else:
-             key_nodes[s] = float(1)
-         sorted(key_nodes.items())
-
+             key_nodes_probability[s] = float(1)
+         sorted(key_nodes_probability.items())
       # BEGIN TO EVALUATE
       # IF THE EVENT OCCURS THE SAME TIME + OTHERWISE MULTIPLICATION
       # https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm Dijkstra
@@ -31,19 +20,19 @@ def recover_input (self):
         nodes, edge, prob_fall_down =  input("Enter a values\n").split()
         convert_float = float(prob_fall_down)
         for i in range(int(nodes)+1, int(edge)+1):
-            if convert_float < key_nodes[i]:
-                key_nodes[i] = convert_float
+            if convert_float < key_nodes_probability [i]:
+                key_nodes_probability [i] = convert_float
         convert_jump -= 1
       success = 1
       # CONVERT PERCENTAGE
-      for k,v in key_nodes.items():
+      for k,v in key_nodes_probability.items():
         if v == float(0):
           continue
         else:
          variation = float(1) - v
          success *= float(variation)
-
-      print(f'{success}')
+         # ABSOLUTE ERROR 10 -6
+      print(f"{success:.6f}")
 
 if __name__ == "__main__":
 
